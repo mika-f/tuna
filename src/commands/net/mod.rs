65dc6db8e531fe82496046051c062b5e8ed03bc5
Natsune Mochizuki <me@natsuneko.cat>
2022-09-30 08:20:27 +0900

    feat: add net_listening

--- src/commands/net/mod.rs
+++ src/commands/net/mod.rs
@@ -1,9 +1,13 @@
 use clap::Parser;
 
+mod listening;
 mod version;
 
 #[derive(Parser)]
 pub enum NetSubCommand {
+    #[clap()]
+    Listening(listening::Args),
+
     #[clap()]
     Version(version::Args),
 }
@@ -13,6 +17,7 @@ pub async fn exec(
     args: NetSubCommand,
 ) -> anyhow::Result<()> {
     match args {
+        NetSubCommand::Listening(args) => listening::exec(globals, args).await,
         NetSubCommand::Version(args) => version::exec(globals, args).await,
     }
 }
