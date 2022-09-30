5de491a3ca6dd1d98e31d818e526d65b358c1fd7
Natsune Mochizuki <me@natsuneko.cat>
2022-09-30 22:49:31 +0900

    feat: add eth_protocolVersion

--- src/commands/eth/mod.rs
+++ src/commands/eth/mod.rs
@@ -1,11 +1,15 @@
 use clap::Parser;
 
 mod chain_id;
+mod protocol_version;
 
 #[derive(Parser)]
 pub enum EthSubCommand {
     #[clap()]
     ChainId(chain_id::Args),
+
+    #[clap()]
+    ProtocolVersion(protocol_version::Args),
 }
 
 pub async fn exec(
@@ -14,5 +18,6 @@ pub async fn exec(
 ) -> anyhow::Result<()> {
     match args {
         EthSubCommand::ChainId(args) => chain_id::exec(globals, args).await,
+        EthSubCommand::ProtocolVersion(args) => protocol_version::exec(globals, args).await,
     }
 }
